36ec7bbad35a3b7819ceaab50ab37ad52225381c
pitikorn <manee-mn@hotmail.com>
2021-04-11 22:52:56 +0700

    feat: remove center middleware

--- config/middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-   settings: {
-     cors: {
-       enabled: true,
-       origin: env('CORS_ORIGIN', 'https://localhost:3000,https://www.thingsofbeam.dev').split(','),
-     },
-   },
- });
